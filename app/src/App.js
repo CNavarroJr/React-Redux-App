8184e48c58b420fa6c9603b5e26161cf71ecbb5d
Junior Navarro <cjuniordse@Juniors-Mac-mini.local>
2020-07-30 18:01:28 -0500

    MVP finished

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Advice from './components/Advice';
+
+export default function App() {
+  return (
+    <div className="App">
+        <h1>Hello World</h1>
+        <Advice />
+    </div>
+  );
+}
+
+
---
+++ app/src/actions/index.js
@@ -0,0 +1,7 @@
+  
+export {
+  FETCH_ADVICE_START,
+  FETCH_ADVICE_SUCCESS,
+  FETCH_ADVICE_FAILURE,
+  getAdvice
+} from './adviceAction';
